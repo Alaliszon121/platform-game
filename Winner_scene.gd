fa371bee5cfc38fa2b9b6f9b303b4610f19e5a7b
Alaliszon121 <isabelle.prince.umh@gmail.com>
2022-11-05 14:06:57 +0100

    design_level_fixed_bug

--- Winner_scene.gd
+++ Winner_scene.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready():
 	var eastereggs = PlayerData.coins
 	var enemies = PlayerData.enemies_killed
-	$Panel/MenuButton.get_popup().add_item("You've collected " + String(eastereggs) + " easteregg(s).")
+	$Panel/MenuButton.get_popup().add_item("You've collected " + String(eastereggs) + " out of 28 easteregg(s).")
 	$Panel/MenuButton.get_popup().add_item("You've killed " + String(enemies) + " enemies.")
 
 func _on_LinkButton_pressed():
